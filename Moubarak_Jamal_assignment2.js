e4a19a50e85b44697f56cd7ac016424a3cd5cd93
Jamal Moubarak <jamalmoubarak@Jamals-MacBook-Pro.local>
2013-02-14 23:52:59 -0500

    Finished writing java.

--- Moubarak_Jamal_assignment2.js
+++ Moubarak_Jamal_assignment2.js
@@ -10,9 +10,6 @@ var timesOfDay = [2, 5, 9, 12, 2],
 	people = ["Myself", "Clients", "Children"],
 	actions = ["homework", "work", "sleep", "wakeup"];
 
-// console.log("My " + people[2] + " told me to " + actions[3] + " at " + timesOfDay[4] + " 		pm");
-
-
 // Function
 var timesOfDay = function(firstNumber,secondNumber){
 	var outcome = firstNumber/secondNumber;
@@ -20,8 +17,6 @@ var timesOfDay = function(firstNumber,secondNumber){
 	return outcome;
 };
 
-
-// Main code
 var divOutcome = timesOfDay(10,5);
 
 console.log("It's " + divOutcome + "am" + "... Should I do " + actions[0] + 
@@ -32,7 +27,7 @@ console.log("It's " + divOutcome + "am" + "... Should I do " + actions[0] +
 			"am to get an early start in the morning? " + 
 			actions[0] + " it is!");
 
-// Boolean Function 
+// Function 
 		var p = true, q = false, r = false;
 
 // Given: (p ∧ q) ∨ r
@@ -48,7 +43,7 @@ console.log("It's 9am... My alarm is annoyingly chirping, " +
 			"5 missed calls from " + people[1] + ". " + "I tell " + people[0] + 
 			" to hit snooze");
 			
-// Number Function
+// Function
 var snooze = 3;
 while (snooze > 0) {
 	console.log("I have " + snooze + " hours before I have to be up");
@@ -57,42 +52,24 @@ while (snooze > 0) {
 
 console.log("It's noon... Time to get out of bed.");
 
+// Function
+var say = function(message) { console.log(message); };
+say("My " + people[2] + " are so happy to see me awake, so I spend a couple hours with them");
 
-/*
-// Project1
-// My variables
-var people = "clients";
-var alarm = "snooze button";
-var sleep = true;
-var calls = 5;
-var kidsYelling = "joyfully yelling \"Daddy is awake\"";
+// Function
+var ending = ["It's", "2", "pm...", "I walk out the door", "clients", "missed calls"];
+console.log (ending[0] + " " + ending[1] + " " + ending[2] + " " + ending[3] + 
+			" trying to figureout what order to visit my " + 
+			ending[4] + " in, which is usually by taking care of the client" + 
+			" I have the most " + ending[5] + " from first." );
 
-// My outputs
-console.log("It's 2am... " + "It is " + sleep + " I should go to sleep " + "so I can" + 
-			" wake up to get an early start, " + "but I stay up doing homework until 5am.");
-console.log("It's 9am... " + "Should I wake up" + " and go with only a couple hours of sleep" + 			" or hit the " + alarm + " and be refreshed?" + " Snooze button it is." );
-console.log("I reach over and grab my phone, " + calls + " missed calls from clients.");
+console.log ("It's midnight...")
+console.log ("It's 5am...")
 
-			
-// My conditionals
-if (sleep === true) {
-	if (calls >= 5) {
-		calls = calls + 30 - (5 * 5) * 1;
-		console.log("If I have " + calls + 
-					" missed calls from clients. " + 
-					"I have to wake up right away. ");
-	} else {
-		console.log("I can sleep all day.");
-	}
-} else {
-	console.log("They can wait" + " as I lay my head back down " + 
-					"to get some more sleep" + " so I will be nice and refreshed " + 
-					"to start my work day.")
-}
 
-// My outputs
-console.log("It's noon... " + "My chilren are so happy to see me that they are " + kidsYelling + 			" so I spend a couple hours with them.");
-console.log("It's 2pm... " + "I walk out the door to visit my " + people + 
-			". It's midnight... It's 5am... " + 
-			"Time to lay my head down so I can do it all over again tomorrow.");
-*/
+
+
+
+
+
+
